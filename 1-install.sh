31f21cf601e5049bb2571287a25071f5f9e522cb
szilard <szilard@users.noreply.github.com>
2015-07-04 20:13:01 -0700

    RJDBC

--- 1-install.sh
+++ 1-install.sh
@@ -4,11 +4,13 @@ Ubuntu Server 14.04 LTS (HVM), SSD Volume Type - ami-5189a661
 m4.xlarge
 
 
+
 sudo apt-get update
 sudo apt-get install mysql-server libmysqlclient-dev htop
 ## Setting up mysql-server-5.5 (5.5.43-0ubuntu0.14.04.1) 
 
 mysql -u root -e "create database bm"
+
 
 
 sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" > /etc/apt/sources.list.d/r.list'
@@ -24,4 +26,18 @@ EOF
 ## trying URL 'http://cran.rstudio.com/src/contrib/RMySQL_0.10.3.tar.gz'
 
 
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+
+sudo R CMD javareconf 
+
+sudo R --vanilla << EOF
+install.packages(c("RJDBC"), repos = "http://cran.rstudio.com")
+EOF
+
+wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.36.tar.gz
+tar xzf mysql-connector-java-5.1.36.tar.gz 
+cp mysql-connector-java-5.1.36/mysql-connector-java-5.1.36-bin.jar .
 
--- README.md
+++ README.md
@@ -2,23 +2,31 @@
 ### Benchmarking R MySQL Data Exchange
 
 
-Insert 1 million rows:
+##### Insert 1 million rows:
 
 RMySQL 11 sec, mysql CL 5 sec - diff: RMySQL writes a csv file first (what else?)
 
+RJDBC runs 9sec for 10K rows (about 100x slower!!) - must be inserting 1 row at a time??
 
-Read 1 mil rows:
+
+##### Read 1 mil rows:
 
 RMySQL 1.8 sec, mysql CL 2.3 - diff: CL dumps the binary data to csv (vs unserialize into R for RMySQL)
 
+RJDBC 4 sec (also `all.equal` show `1e-7` diff in floats!)
 
 
-Write 1 row:
+
+##### Write 1 row:
 
 RMySQL: 15 ms (millisec) including overwrite table
 
+RJDBC 13 ms 
 
-Read 1 row:
+
+##### Read 1 row:
 
 RMySQL 0.4 ms
+
+RJDBC 3 ms (10x slower)
 
